81df757f52a82c216abc5830477832bec7464780
GP <gregpetropoulos@yahoo.com>
2021-10-29 08:04:52 -0400

    Added Navbar, PropTypes, defaultProps

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layouts/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>hellooo world</h1>
+        <Navbar />
+        <h1>Hello</h1>
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Navbar extends Component {
+  static defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+  };
+  //   This is default runs if no props passed to <Navbar/> in app.js
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={this.props.icon} />
+          {this.props.title}
+        </h1>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
