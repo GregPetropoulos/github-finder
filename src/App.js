b307c08067a959eecd84b8e2518dfdab61faea37
GP <gregpetropoulos@yahoo.com>
2021-10-29 16:26:29 -0400

    Added a UserItem Component and made a hardcoded call to the github api to check of the state was updating the ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layouts/Navbar';
-
+import UserItem from './components/users/UserItem';
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Navbar />
+        <UserItem/>
         <h1>Hello</h1>
       </div>
     );
