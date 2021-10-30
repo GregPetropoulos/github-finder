3e24a9d9a0751b34fb6c1464ae4786c6f8392512
GP <gregpetropoulos@yahoo.com>
2021-10-30 14:18:51 -0400

    Added .env.local variables and used the variables in the parameters of the axios call.

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layouts/Spinner';
 import PropTypes from 'prop-types';
