0c9a1048cddd2d5d6cfcd385ea3959dd1d844c66
GP <gregpetropoulos@yahoo.com>
2021-11-07 07:39:56 -0500

    Finished the github state management.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Fragment, useState } from 'react';
 import './App.css';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
-import axios from 'axios';
 import Search from './components/users/Search';
 import Alert from './components/layouts/Alert';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -12,9 +11,6 @@ import GithubState from './context/github/GithubState';
 
 
 const App = () => {
-
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
   // async componentDidMount() {
@@ -31,15 +27,7 @@ const App = () => {
   //Get a single Github user
   
   //Get users Repos
-  const getUserRepos = async (username) => {
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET_ID}`
-    );
-    setRepos(res.data);
-    setLoading(false);
-  };
-
+  
   //Clear users from the state
   //Setting alert when nothing typed into search bar
   const showAlert = (msg, type) => {
@@ -68,18 +56,11 @@ const App = () => {
                   </Fragment>
                 )}
               />
-              <Route exact path='/about' component={About} />
+              <Route exact path='/about' component={ About }/>
               <Route
                 exact
                 path='/user/:login'
-                render={(props) => (
-                  <User
-                    {...props}
-                    getUserRepos={getUserRepos}
-                    repos={repos}
-                  />
-                )}
-              />
+                component ={ User }/>
             </Switch>
           </div>
         </div>
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,15 +1,14 @@
 import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layouts/Spinner';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Repos from '../repos/Repos';
 import GithubContext from '../../context/github/githubContext';
 
-export const User = ({ repos, getUserRepos, match }) => {
+export const User = ({  match }) => {
   
   const githubContext = useContext(GithubContext);
 
-  const  {user, loading, getUser }= githubContext;
+  const  {user, loading, getUser,repos, getUserRepos, }= githubContext;
 
   useEffect(() => {
     getUser(match.params.login);
@@ -104,10 +103,5 @@ export const User = ({ repos, getUserRepos, match }) => {
       </Fragment>
     );
   }
-User.propTypes = {
-  getUserRepos: PropTypes.func.isRequired,
-  repos:PropTypes.array.isRequired,
-
-};
 
 export default User;
