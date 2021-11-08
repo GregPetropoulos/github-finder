be13e8e8ddb56c43f6da59958531d125102013e9
GP <gregpetropoulos@yahoo.com>
2021-11-08 06:22:29 -0500

    Created a home page route and not found page and  route.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Fragment} from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layouts/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
-import Alert from './components/layouts/Alert';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import About from './components/pages/About';
 import User from './components/users/User';
+import Alert from './components/layouts/Alert';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 
 const App = () => {
-
   return (
     <GithubState>
       <AlertState>
@@ -20,20 +19,13 @@ const App = () => {
           <div className='App'>
             <Navbar />
             <div className='container'>
-              <Alert/>
+              <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search/>
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
+                <Route exact path='/Home' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component ={NotFound}/>
               </Switch>
             </div>
           </div>
